4ca3ac84329a169533b36288912362525fb5d14e
Krzem <krzem5.dev@gmail.com>
2021-02-27 13:39:43 +0100

    Push Update 02/27/2021, 13:39:35

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 cls
 if exist build rmdir /s /q build
 mkdir build
-nasm -f bin boot.asm -o build/boot.bin&&cls&&qemu-system-x86_64 -drive file=build/boot.bin,format=raw,index=0,media=disk&&del *.bin
+nasm -f bin src/boot.asm -o build/boot.bin&&cls&&qemu-system-x86_64 -drive file=build/boot.bin,format=raw,index=0,media=disk&&del *.bin
